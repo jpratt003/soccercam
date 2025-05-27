5a18ea1e0e39bd08ca88aa5335e5c5cae9905572
John Pratt <john.pratt@patternlabs.ai>
2025-05-26 21:04:46 -0600

    Adding better initial docs. (#2)

--- crop.sh
+++ crop.sh
@@ -1,1 +1,1 @@
- ffmpeg -i eljebelcam2.mp4 -ss 00:44:00 -t 00:00:20 -vcodec copy -acodec copy porter_pk.mp4
+ ffmpeg -i videos/stenger3combined.mp4 -ss 00:02:00 -t 00:48:00 -vcodec copy -acodec copy videos/stegner3combinededit.mp4
