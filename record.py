c2dfcb6ccf9a50ae5767793013b963734aa963d0
John Pratt <john.pratt@patternlabs.ai>
2025-08-03 18:54:38 -0600

    Several cleanup changes and a working preview. (#3)
    * Several cleanup changes and a working preview.
    
    * Revert "Create pylint.yml"
    
    This reverts commit 16c826f2a7e358904fb20ec4fd0dba83b9fab1dd.

--- record.py
+++ record.py
@@ -1,38 +1,102 @@
 #!/usr/bin/env python3
+# MIT License
+# 
+# Copyright (c) 2025 John Pratt
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 import time
+import argparse
+import os
 
 from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
+from libcamera import Transform
 
-cam1 = Picamera2(0)
-# cam1.start_preview(Preview.QTGL, x=100,y=300,width=400,height=300)
+class CameraRecorder:
+    def __init__(self, camera_id, output_file):
+        self.camera = Picamera2(camera_id)
+        self.output_file = output_file
+        self.encoder = H264Encoder(10000000)
+        self.output = FfmpegOutput(output_file)
 
-video_config1= cam1.create_video_configuration()
-cam1.configure(video_config1)
+    def configure(self, include_preview=False):
+        video_config = self.camera.create_video_configuration()
+        
+        #main_config.transform = Transform(hflip=True, vflip=True)
+        self.camera.configure(video_config)
 
-encoder1 = H264Encoder(10000000)
-output1 = FfmpegOutput('testcam1.mp4')
+    def start_recording(self):
+        self.camera.start_recording(self.encoder, self.output)
 
-cam2 = Picamera2(1)
-# cam2.start_preview(Preview.QTGL, x=500,y=300,width=400,height=300)
+    def stop_recording(self):
+        self.camera.stop_recording()
 
-video_config2 = cam2.create_video_configuration()
-cam2.configure(video_config2)
+    def start_preview(self, preview_type=Preview.QT):
+        self.camera.start_preview(preview_type, hflip=True, vflip=True)
 
-encoder2= H264Encoder(10000000)
-output2 = FfmpegOutput('testcam2.mp4')
+    def stop_preview(self):
+        self.camera.stop_preview()
 
-cam1.start_recording(encoder1, output1)
-cam2.start_recording(encoder2, output2)
+    def stop(self):
+        self.camera.stop()
 
-time.sleep(10)
+def main():
+    parser = argparse.ArgumentParser(description="Record video from two cameras.")
+    parser.add_argument('--output1', type=str, default='left_camera.mp4', help='Output filename for camera 1')
+    parser.add_argument('--output2', type=str, default='right_camera.mp4', help='Output filename for camera 2')
+    parser.add_argument('--duration', type=int, default=90, help='Recording duration in minutes')
+    parser.add_argument('--preview', action='store_true', help='Enable camera previews')
+    parser.add_argument('--directory', type=str, default='videos', help='Directory to save videos')
 
-cam2.stop_recording()
-cam1.stop_recording()
+    args = parser.parse_args()
 
-cam1.stop_preview()
-cam2.stop_preview()
+    # Ensure output directory exists
+    os.makedirs(args.directory, exist_ok=True)
+    output1_path = os.path.join(args.directory, args.output1)
+    output2_path = os.path.join(args.directory, args.output2)
 
-cam2.stop()
-cam1.stop()
+    cam1 = CameraRecorder(0, output1_path)
+    cam2 = CameraRecorder(1, output2_path)
+
+    if args.preview:
+        cam1.start_preview()
+        cam2.start_preview()
+
+    cam1.configure()
+    cam2.configure()
+
+    cam1.start_recording()
+    cam2.start_recording()
+
+    time.sleep(args.duration * 60)
+
+    cam2.stop_recording()
+    cam1.stop_recording()
+
+    if args.preview:
+        cam1.stop_preview()
+        cam2.stop_preview()
+
+    cam2.stop()
+    cam1.stop()
+
+
+if __name__ == "__main__":
+    main()
