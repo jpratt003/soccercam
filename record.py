4bf373b24a87ab406d13c84358bcc8a89f7355c7
John Pratt <john.pratt@patternlabs.ai>
2025-08-03 19:11:24 -0600

    Additional clean and including flake8 config.

--- record.py
+++ record.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # MIT License
-# 
+#
 # Copyright (c) 2025 John Pratt
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,17 +29,21 @@ from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 from libcamera import Transform
 
+
 class CameraRecorder:
-    def __init__(self, camera_id, output_file):
+    def __init__(self, camera_id, output_file, hflip=True, vflip=True):
         self.camera = Picamera2(camera_id)
         self.output_file = output_file
         self.encoder = H264Encoder(10000000)
         self.output = FfmpegOutput(output_file)
+        self.hflip = hflip
+        self.vflip = vflip
 
-    def configure(self, include_preview=False):
-        video_config = self.camera.create_video_configuration()
-        
-        #main_config.transform = Transform(hflip=True, vflip=True)
+    def configure(self):
+        video_config = self.camera.create_video_configuration(
+            transform=Transform(hflip=self.hflip, vflip=self.vflip)
+        )
+
         self.camera.configure(video_config)
 
     def start_recording(self):
@@ -49,7 +53,8 @@ class CameraRecorder:
         self.camera.stop_recording()
 
     def start_preview(self, preview_type=Preview.QT):
-        self.camera.start_preview(preview_type, hflip=True, vflip=True)
+        # self.camera.start_preview(preview_type, hflip=True, vflip=True)
+        self.camera.start_preview(preview_type)
 
     def stop_preview(self):
         self.camera.stop_preview()
@@ -57,13 +62,28 @@ class CameraRecorder:
     def stop(self):
         self.camera.stop()
 
+
 def main():
     parser = argparse.ArgumentParser(description="Record video from two cameras.")
-    parser.add_argument('--output1', type=str, default='left_camera.mp4', help='Output filename for camera 1')
-    parser.add_argument('--output2', type=str, default='right_camera.mp4', help='Output filename for camera 2')
-    parser.add_argument('--duration', type=int, default=90, help='Recording duration in minutes')
-    parser.add_argument('--preview', action='store_true', help='Enable camera previews')
-    parser.add_argument('--directory', type=str, default='videos', help='Directory to save videos')
+    parser.add_argument(
+        "--output1",
+        type=str,
+        default="left_camera.mp4",
+        help="Output filename for camera 1",
+    )
+    parser.add_argument(
+        "--output2",
+        type=str,
+        default="right_camera.mp4",
+        help="Output filename for camera 2",
+    )
+    parser.add_argument(
+        "--duration", type=int, default=90, help="Recording duration in minutes"
+    )
+    parser.add_argument("--preview", action="store_true", help="Enable camera previews")
+    parser.add_argument(
+        "--directory", type=str, default="videos", help="Directory to save videos"
+    )
 
     args = parser.parse_args()
 
